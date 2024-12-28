ba26fcc7987e426007a318024cc13c5d2ecb085a
Your Name <michaelalex03@outlook.com>
2024-12-28 00:18:51 -0600

    got rid of unused post and get default routes in sets.js

--- backend/routes/api/sets.js
+++ backend/routes/api/sets.js
@@ -2,10 +2,7 @@ const express = require('express')
 const router = express.Router();
 const setsController = require('../../controller/api/setsController');
 
-
 router.route('/')
-    .get()
-    .post()
     .delete(setsController.removeAllSets)
 
 router.route('/:workoutId')
