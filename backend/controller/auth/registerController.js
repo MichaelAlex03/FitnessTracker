d7077063bfc26266c4094fc78c0959a91afc3d40
Your Name <michaelalex03@outlook.com>
2025-01-10 23:46:24 -0600

    small changes no comments needed

--- backend/controller/auth/registerController.js
+++ backend/controller/auth/registerController.js
@@ -3,18 +3,18 @@ const pg = require('../../model/sql')
 
 const handleNewUser = async (req, res) => {
     const { user, pwd } = req.body;
-    if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required'});
+    if (!user || !pwd) return res.status(400).json({ message: 'Username and password are required' });
 
     //check for duplicate usernames in the db
     const duplicate = await pg.findUser(user);
     if (duplicate.rows.length !== 0) return res.sendStatus(409); //conflict
-    try{
+    try {
         //encrypt password
         const hashedPwd = await bcrypt.hash(pwd, 10);
-        const result = await pg.createUser(user, hashedPwd)
-        res.status(201).json({ 'success': `New user ${user} created!`});
-    }catch(err){
-        res.status(500).json({ 'message': err.message})
+        await pg.createUser(user, hashedPwd)
+        res.status(201).json({ 'success': `New user ${user} created!` });
+    } catch (err) {
+        res.status(500).json({ 'message': err.message })
     }
 }
 
