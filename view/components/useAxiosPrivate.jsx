09d63fb5787791a82bb60953b08f24fdf9550233
Your Name <michaelalex03@outlook.com>
2025-01-18 21:52:11 -0600

    handled api request requiring accessToken and if failed will hit refresh endpoint to get new accessToken

--- view/components/useAxiosPrivate.jsx
+++ view/components/useAxiosPrivate.jsx
@@ -2,3 +2,43 @@ import axios, { axiosPrivate } from "../api/axios";
 import { useEffect } from "react";
 import useRefreshToken from "./useRefreshToken";
 import useAuth from "./useAuth";
+
+const useAxiosPrivate = () => {
+    const refresh = useRefreshToken()
+    const { auth } = useAuth();
+
+    useEffect(() => {
+
+        const requestIntercept = axiosPrivate.interceptors.request.use(
+            config => {
+                if (!config.headers['Authorization']) {
+                    config.headers['Authorization'] = `Bearer ${auth?.accessToken}`;
+                }
+                return config
+            }, (error) => Promise.reject(error)
+        );
+
+        const responseIntercept = axiosPrivate.interceptors.response.use(
+            response => response,
+            async (error) => {
+                const prevRequeust = error?.config;
+                if (error?.response?.status === 403 && !prevRequeust?.sent) {
+                    prevRequeust.sent = true
+                    const newAccessToken = await refresh();
+                    prevRequeust.headers['Authorization'] = `Bearer ${newAccessToken}`;
+                    return axiosPrivate(prevRequeust);
+                }
+                return Promise.reject(error)
+            }
+        )
+
+        return () => {
+            axiosPrivate.interceptors.response.eject(responseIntercept);
+            axiosPrivate.interceptors.request.eject(requestIntercept);
+        }
+    }, [auth, refresh])
+
+    return axiosPrivate
+}
+
+export default useAxiosPrivate
