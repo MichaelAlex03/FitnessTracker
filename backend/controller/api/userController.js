1d426a3b8532b8170ce7702978a19d257110b2d0
Your Name <michaelalex03@outlook.com>
2025-01-29 13:39:24 -0600

    added findUserbyId query function in sql.js

--- backend/controller/api/userController.js
+++ backend/controller/api/userController.js
@@ -1,12 +1,20 @@
-const getUserInfo = (req, res) => {
+const pg = require('../../model/sql')
 
+const getUserInfo = (req, res) => {
+    const { id } = req.params;
+    
+    try {
+        const userInfo = pg.
+    } catch (error) {
+        res.sendStatus(500);
+    }
 }
 
 const updateUserInfo = (req, res) => {
 
 }
 
-module.exports = { 
+module.exports = {
     getUserInfo,
     updateUserInfo,
 };
--- backend/model/sql.js
+++ backend/model/sql.js
@@ -1,8 +1,8 @@
 const db = require('.././config/dbConn');
 
 const updateUser = async (user, refreshToken) => {
-    return await db.query('UPDATE users SET refresh_token = $1 WHERE user_name = $2', [refreshToken, user])
-}
+    return await db.query('UPDATE users SET refresh_token = $1 WHERE user_name = $2', [refreshToken, user]);
+};
 
 const updateSets = async (sets) => {
     sets.map(set => {
@@ -10,33 +10,37 @@ const updateSets = async (sets) => {
             'UPDATE workout_sets SET exercise_reps = $1, exercise_weight = $2 WHERE id = $3', [set.exercise_reps, set.exercise_weight, set.id]
         );
     });
-}
+};
 
 //---------------------------- Get Routes Queries ------------------------------//
 const findUser = async (user) => {
-    return await db.query('SELECT * FROM users WHERE user_name = $1', [user])
-}
+    return await db.query('SELECT * FROM users WHERE user_name = $1', [user]);
+};
+
+const findUserbyId = async (userId) => {
+    return await db.query('SELECT * FROM users WHERE id = $1', [userId]);
+};
 
 const findRefreshToken = async (refreshToken) => {
-    return await db.query('SELECT * FROM users WHERE refresh_token = $1', [refreshToken])
-}
+    return await db.query('SELECT * FROM users WHERE refresh_token = $1', [refreshToken]);
+};
 
 const getWorkouts = (userId) => {
     console.log(userId)
     return db.query('SELECT * FROM workouts WHERE user_id = $1', [userId]);
-}
+};
 
 const getAllExercises = async () => {
     return await db.query('SELECT * FROM exercises');
-}
+};
 
 const getWorkoutExercises = async (workoutId) => {
     return await db.query('SELECT * FROM user_exercises WHERE workout_id = $1', [workoutId]);
-}
+};
 
 const getWorkoutSets = async (workoutId) => {
     return await db.query('SELECT * FROM workout_sets WHERE workout_id = $1', [workoutId]);
-}
+};
 
 const getAllSetsForExercise = async (userId, exerciseName) => {
     return await db.query(
@@ -64,11 +68,11 @@ const getAllSetsForExercise = async (userId, exerciseName) => {
 
 const createUser = async (user, pwd) => {
     return await db.query('INSERT INTO users (user_name, user_pass) VALUES( $1, $2)', [user, pwd]);
-}
+};
 
 const createWorkout = async (workoutName, userId) => {
     return await db.query('INSERT INTO workouts (workout_name, user_id) VALUES ($1, $2) RETURNING *', [workoutName, userId]);
-}
+};
 
 const createWorkoutExercises = async (workoutId, selectedExercises) => {
     try {
@@ -80,38 +84,38 @@ const createWorkoutExercises = async (workoutId, selectedExercises) => {
     } catch (err) {
         return err;
     }
-}
+};
 
 //This function used when workout is initially created. addSet is used for all future set creations
 const createSet = async (exercise) => {
     return await db.query('INSERT INTO workout_sets (exercise_id, exercise_reps, exercise_weight, workout_id) VALUES ($1, $2, $3, $4)',
         [exercise.rows[0].id, 0, 0, exercise.rows[0].workout_id]
-    )
-}
+    );
+};
 
 const addSet = async (exercise, workoutId) => {
     return await db.query('INSERT INTO workout_sets (exercise_id, exercise_reps, exercise_weight, workout_id) VALUES ($1, $2, $3, $4)',
         [exercise.id, 0, 0, workoutId]
-    )
-}
+    );
+};
 
 //---------------------------- Delete Routes Queries ------------------------------//
 
 const removeAllSets = async (workoutId) => {
     return await db.query('DELETE FROM workout_sets WHERE workout_id = $1', [workoutId]);
-}
+};
 
 const removeAllExercises = async (workoutId) => {
     return await db.query('DELETE FROM user_exercises WHERE workout_id = $1', [workoutId]);
-}
+};
 
 const removeWorkout = async (workoutId) => {
     return await db.query('DELETE FROM workouts WHERE id = $1', [workoutId]);
-}
+};
 
 const deleteSet = async (setId) => {
     return await db.query('DELETE FROM workout_sets WHERE id = $1', [setId]);
-}
+};
 
 const deleteExercise = async (exerciseId) => {
     try {
@@ -121,12 +125,13 @@ const deleteExercise = async (exerciseId) => {
     } catch (err) {
         return err;
     }
-}
+};
 
 //Exports
 
 module.exports = {
     findUser,
+    findUserbyId,
     findRefreshToken,
     updateUser,
     createUser,
