d488abb4767ef94d54b12d8a9b5309d8bb5b494d
Michael A Lleverino <michaelalleverino@Michaels-MacBook-Pro.local>
2025-01-31 19:15:13 -0600

    added export to user route and updated git ignore in view

--- backend/routes/api/user.js
+++ backend/routes/api/user.js
@@ -5,3 +5,5 @@ const userController = require('../../controller/api/userController');
 router.route('/:userName')
     .get(userController.getUserInfo)
     .patch(userController.updateUserInfo)
+
+module.exports = router;
