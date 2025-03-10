b80ba79642ef55c854efc90ed02a5ebb700dd0bb
MichaelAlex03 <michaelalex03@outlook.com>
2025-03-10 12:06:48 -0500

    added schemas for sql tables

--- backend/model/tables.sql
+++ backend/model/tables.sql
@@ -1,3 +1,36 @@
-CREATE users (
-
+-- Users table
+CREATE TABLE users (
+    id NOT NULL PRIMARY KEY,
+    user_name TEXT,
+    user_pass TEXT,
+    refresh_token TEXT
 );
+
+--Workouts table
+CREATE TABLE workouts (
+    id NOT NULL PRIMARY KEY,
+    workout_name TEXT,
+    user_id FOREIGN KEY REFERENCES users(id),
+);
+
+--User exercises table
+CREATE TABLE user_exercises (
+    id NOT NULL PRIMARY KEY,
+    exercise_name TEXT,
+    workout_id FOREIGN KEY REFERENCES workouts(id)
+);
+
+--Workout sets table
+CREATE TABLE workout_sets (
+    id NOT NULL PRIMARY KEY,
+    exercise_id INT,
+    exercise_reps INT,
+    exercise_weight INT,
+    workout_id FOREIGN KEY REFERENCES workouts(id)
+);
+
+--Exercises table (List of exercises to choose from)
+CREATE TABLE exercises (
+    id PRIMARY KEY,
+    exercise_name TEXT,
+)
